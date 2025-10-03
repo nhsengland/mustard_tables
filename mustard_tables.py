973af26bfbf3512fd4bd1f50bdecce09f7a07daa
martindata999 <martin.bloyce2@nhs.net>
2025-10-03 14:13:40 +0100

    Fixes to Cat2 MTD and brought in Cat2 YTD

--- mustard_tables.py
+++ mustard_tables.py
@@ -9,9 +9,17 @@ data = pd.read_csv("data/mustard_tables.csv")
 # Bring in cat2 data
 cat2_data = pd.read_csv("data/em23c_output.csv")
 
+# Bring in cat2 plans data
+cat2_plans = pd.read_csv("data/cat2_plans.csv")
+
+# # Get date into mmm-yy format
+# cat2_plans['date'] = pd.to_datetime(data['date'], format='%d/%m/%Y').dt.strftime('%b-%y')
+
 # Rename monthdate as date and orgcode as org_name
 cat2_data = cat2_data.rename(columns={'monthformatted': 'date',
-                                      'orgcode': 'org_name'})
+                                      'orgcode': 'org_name',
+                                      'value': 'actual'
+                                      })
 # Rename org_name values
 cat2_data['org_name'] = cat2_data['org_name'].replace({'RYD': ' SECAM',
                                                        'RYE': ' SCAS',
@@ -23,13 +31,33 @@ cat2_data = cat2_data[cat2_data['monthstart'] == latest_monthstart]
 # Drop monthstart column
 cat2_data = cat2_data.drop(columns=['monthstart'])
 # Create metric_name, variable and metric_type columns, to match mustard data
-cat2_data['metric_name'] = 'Cat2 (MTD)'
+# cat2_data['metric_name'] = 'Cat2 (MTD)'
 cat2_data['variable'] = 'value'
-cat2_data['metric_type'] = 'actual'
+# cat2_data['metric_type'] = 'actual'
 # Amend value column to be numeric minutes
-cat2_data['value'] = pd.to_numeric(cat2_data['value'], errors='coerce') / 60
+cat2_data['actual'] = pd.to_numeric(cat2_data['actual'], errors='coerce') / 60
 # Remove white space from start of org_name values
 cat2_data['org_name'] = cat2_data['org_name'].str.strip()
+
+# Amend cat2_plans value column to be numeric minutes
+cat2_plans['plan'] = pd.to_numeric(cat2_plans['plan'], errors='coerce') / 60
+
+# Join cat2 plans to cat2 data
+cat2_data = pd.merge(cat2_data, cat2_plans, 
+                     on=['date', 'org_name', 'metric_name'], 
+                     how='left'
+                     )
+
+# Unpivot cat2_data to show plan and value in variable column
+cat2_data = pd.melt(cat2_data,
+                    id_vars=['date', 'org_name', 'metric_name', 'variable'],
+                    value_vars=['actual', 'plan'],
+                    var_name='metric_type',
+                    value_name='value'
+                    )
+
+# Make variable column values same as metric_type values
+cat2_data['variable'] = cat2_data['metric_type']
 
 # Union the two dataframes together
 data = pd.concat([data, cat2_data], ignore_index=True)
@@ -71,6 +99,7 @@ metric_groups = {
     '4hrs': 'UEC',
     '4hrs (MTD)': 'UEC',
     'Cat2 (MTD)': 'UEC',
+    'Cat2 (YTD)': 'UEC',
     '52ww performance': 'Elective',
     '52ww performance (MTD)': 'Elective',
     'Cancer 62d': 'Elective',
@@ -98,6 +127,7 @@ elective_data = data[data['metric_group'] == 'Elective'].copy()
 # Set up list for metrics where higher is worse
 higher_is_worse_metrics = ['12hrs',
                            'Cat2 (MTD)',
+                           'Cat2 (YTD)',
                            '52ww performance',
                            '52ww performance (MTD)',
                            'DM01'
@@ -117,56 +147,56 @@ higher_is_better_metrics = ['Cancer 62d',
 higher_is_worse_metrics = [metric + ' - ' + date for metric in higher_is_worse_metrics for date in data['date'].unique()]
 higher_is_better_metrics = [metric + ' - ' + date for metric in higher_is_better_metrics for date in data['date'].unique()]
 
-# Create list of metrics to ignore for actual vs plan comparison
-ignore_metrics = ['RTT performance (MTD) - ' + date for date in data['date'].unique()]
-ignore_metrics += ['4hrs (MTD) - ' + date for date in data['date'].unique()]
-ignore_metrics += ['52ww performance (MTD) - ' + date for date in data['date'].unique()]
+# # Create list of metrics to ignore for actual vs plan comparison
+# ignore_metrics = ['RTT performance (MTD) - ' + date for date in data['date'].unique()]
+# ignore_metrics += ['4hrs (MTD) - ' + date for date in data['date'].unique()]
+# ignore_metrics += ['52ww performance (MTD) - ' + date for date in data['date'].unique()]
 
-# Set order of columns for final output - note this works even when splitting by metric_group
-col_custom_order_uec = pd.MultiIndex.from_tuples([
-    # UEC metrics
-    ('12hrs - ' + data['date'].unique()[0], 'actual'),
-    ('12hrs - ' + data['date'].unique()[0], 'plan'),
-    ('4hrs - ' + data['date'].unique()[0], 'actual'),
-    ('4hrs - ' + data['date'].unique()[0], 'plan'),
-    ('4hrs (MTD) - ' + data['date'].unique()[0], 'actual'),
-    ('4hrs (MTD) - ' + data['date'].unique()[0], 'plan'),
-    ('Cat2 (MTD) - ' + data['date'].unique()[0], 'actual'),
-    ('Cat2 (MTD) - ' + data['date'].unique()[0], 'plan'),
-    ('12hrs - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ('4hrs - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ('4hrs (MTD) - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ('Cat2 (MTD) - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ])
-col_custom_order_elective = pd.MultiIndex.from_tuples([
-    # Elective metrics
-    ('RTT performance - ' + data['date'].unique()[0], 'actual'),
-    ('RTT performance - ' + data['date'].unique()[0], 'plan'),
-    ('RTT performance - ' + data['date'].unique()[0], 'numerator_actual'),
-    ('RTT performance - ' + data['date'].unique()[0], 'numerator_plan'),
-    ('RTT performance (MTD) - ' + data['date'].unique()[0], 'actual'),
-    ('RTT performance (MTD) - ' + data['date'].unique()[0], 'plan'),
-    ('52ww performance - ' + data['date'].unique()[0], 'actual'),
-    ('52ww performance - ' + data['date'].unique()[0], 'plan'),
-    ('52ww performance (MTD) - ' + data['date'].unique()[0], 'actual'),
-    ('52ww performance (MTD) - ' + data['date'].unique()[0], 'plan'),
-    ('Time to first OPA - ' + data['date'].unique()[0], 'actual'),
-    ('Time to first OPA - ' + data['date'].unique()[0], 'plan'),
-    ('Cancer FDS - ' + data['date'].unique()[0], 'actual'),
-    ('Cancer FDS - ' + data['date'].unique()[0], 'plan'),
-    ('Cancer 62d - ' + data['date'].unique()[0], 'actual'),
-    ('Cancer 62d - ' + data['date'].unique()[0], 'plan'),
-    ('DM01 - ' + data['date'].unique()[0], 'actual'),
-    ('DM01 - ' + data['date'].unique()[0], 'plan'),
-    ('RTT performance - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ('RTT performance (MTD) - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ('52ww performance - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ('DM01 - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ('52ww performance (MTD) - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ('Time to first OPA - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ('Cancer FDS - ' + data['date'].unique()[0], 'actual_gt_plan'),
-    ('Cancer 62d - ' + data['date'].unique()[0], 'actual_gt_plan')
-    ])
+# # Set order of columns for final output - note this works even when splitting by metric_group
+# col_custom_order_uec = pd.MultiIndex.from_tuples([
+#     # UEC metrics
+#     ('12hrs - ' + data['date'].unique()[0], 'actual'),
+#     ('12hrs - ' + data['date'].unique()[0], 'plan'),
+#     ('4hrs - ' + data['date'].unique()[0], 'actual'),
+#     ('4hrs - ' + data['date'].unique()[0], 'plan'),
+#     ('4hrs (MTD) - ' + data['date'].unique()[0], 'actual'),
+#     ('4hrs (MTD) - ' + data['date'].unique()[0], 'plan'),
+#     ('Cat2 (MTD) - ' + data['date'].unique()[0], 'actual'),
+#     ('Cat2 (MTD) - ' + data['date'].unique()[0], 'plan'),
+#     ('12hrs - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ('4hrs - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ('4hrs (MTD) - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ('Cat2 (MTD) - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ])
+# col_custom_order_elective = pd.MultiIndex.from_tuples([
+#     # Elective metrics
+#     ('RTT performance - ' + data['date'].unique()[0], 'actual'),
+#     ('RTT performance - ' + data['date'].unique()[0], 'plan'),
+#     ('RTT performance - ' + data['date'].unique()[0], 'numerator_actual'),
+#     ('RTT performance - ' + data['date'].unique()[0], 'numerator_plan'),
+#     ('RTT performance (MTD) - ' + data['date'].unique()[0], 'actual'),
+#     ('RTT performance (MTD) - ' + data['date'].unique()[0], 'plan'),
+#     ('52ww performance - ' + data['date'].unique()[0], 'actual'),
+#     ('52ww performance - ' + data['date'].unique()[0], 'plan'),
+#     ('52ww performance (MTD) - ' + data['date'].unique()[0], 'actual'),
+#     ('52ww performance (MTD) - ' + data['date'].unique()[0], 'plan'),
+#     ('Time to first OPA - ' + data['date'].unique()[0], 'actual'),
+#     ('Time to first OPA - ' + data['date'].unique()[0], 'plan'),
+#     ('Cancer FDS - ' + data['date'].unique()[0], 'actual'),
+#     ('Cancer FDS - ' + data['date'].unique()[0], 'plan'),
+#     ('Cancer 62d - ' + data['date'].unique()[0], 'actual'),
+#     ('Cancer 62d - ' + data['date'].unique()[0], 'plan'),
+#     ('DM01 - ' + data['date'].unique()[0], 'actual'),
+#     ('DM01 - ' + data['date'].unique()[0], 'plan'),
+#     ('RTT performance - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ('RTT performance (MTD) - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ('52ww performance - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ('DM01 - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ('52ww performance (MTD) - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ('Time to first OPA - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ('Cancer FDS - ' + data['date'].unique()[0], 'actual_gt_plan'),
+#     ('Cancer 62d - ' + data['date'].unique()[0], 'actual_gt_plan')
+#     ])
 
 # Loop through dataframes
 for df in [elective_data, uec_data]:
@@ -196,23 +226,6 @@ for df in [elective_data, uec_data]:
         if col != 'org_name' or col != 'actual_gt_plan' or col.startswith('Cat2 (MTD)'):
             pivot_table[col] = pd.to_numeric(pivot_table[col], errors='coerce')
 
-    
-    def minutes_to_mmss(x):
-        try:
-            if pd.isna(x):
-                return ""
-            minutes = int(x)
-            seconds = int(round((x - minutes) * 60))
-            return f"{minutes:02d}:{seconds:02d}"
-        except Exception:
-            return ""
-
-    # In your loop, after creating the pivot_table, format Cat2 (MTD) columns:
-    if 'Cat2 (MTD)' in df['metric_name'].values:
-        for col in pivot_table.columns:
-            if col[0].startswith('Cat2 (MTD)') and col[1] == 'actual':
-                pivot_table[col] = pivot_table[col].apply(minutes_to_mmss)
-
     # Create column for each metric_name to show where actual is worse than plan
     def actual_greater_than_plan(row, metric):
         if metric in higher_is_better_metrics:
@@ -231,6 +244,22 @@ for df in [elective_data, uec_data]:
     # Apply the function to each metric_name
     for metric in df['metric_date'].unique():
         pivot_table[(metric, 'actual_gt_plan')] = pivot_table.apply(lambda row: actual_greater_than_plan(row, metric), axis=1)
+
+    def minutes_to_mmss(x):
+        try:
+            if pd.isna(x):
+                return ""
+            minutes = int(x)
+            seconds = int(round((x - minutes) * 60))
+            return f"{minutes:02d}:{seconds:02d}"
+        except Exception:
+            return ""
+
+    # After creating the pivot_table, format Cat2 (MTD) columns:
+    if 'Cat2 (MTD)' in df['metric_name'].values:
+        for col in pivot_table.columns:
+            if col[0].startswith('Cat2') and col[1] in ['actual', 'plan']:
+                pivot_table[col] = pivot_table[col].apply(minutes_to_mmss)
 
     # Function to apply mustard background and thicker left border to cells where actual is worse than plan
     def style_actual_vs_plan(row):
